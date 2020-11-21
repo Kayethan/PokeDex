466b2386689bcacc84a2d074b922e3b8d4ab1ce1
Lathuys <lathuys@outlook.com>
2020-11-21 11:41:31 +0100

    Fix package name

--- app/src/androidTest/java/com/kayethan/pokedex/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/kayethan/pokedex/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.pokedex
+package com.kayethan.pokedex
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.pokedex", appContext.packageName)
+        assertEquals("com.kayethan.pokedex", appContext.packageName)
     }
 }
--- app/src/main/java/com/kayethan/pokedex/MainActivity.kt
+++ app/src/main/java/com/kayethan/pokedex/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.pokedex
+package com.kayethan.pokedex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
