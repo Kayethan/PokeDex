90c6b92b38b682c8af2a626cc5199e26fc3872eb
Lathuys <lathuys@outlook.com>
2020-11-21 12:48:24 +0100

    Added app menu. Started working Pokemon list

--- app/src/main/java/com/kayethan/pokedex/MainActivity.kt
+++ app/src/main/java/com/kayethan/pokedex/MainActivity.kt
@@ -2,10 +2,59 @@ package com.kayethan.pokedex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import com.kayethan.pokedex.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var menu: Menu
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        Log.i("test", "onSaveInstanceState")
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        Log.i("test", "onRestoreInstanceState")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        if (menu != null)
+            this.menu = menu
+        menuInflater.inflate(R.menu.app_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_favorites -> {
+                item.isChecked = !item.isChecked
+
+                if (item.isChecked)
+                {
+                    item.setIcon(R.drawable.ic_favorite_checked)
+                }
+                else
+                {
+                    item.setIcon(R.drawable.ic_favorite_unchecked)
+                }
+            }
+            R.id.action_filter_types -> {
+
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
